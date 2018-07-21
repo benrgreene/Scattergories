ef7634391f97e1d04799ab2e97ef56833362297a
Ben Greene <benrgreene@gmail.com>
2018-07-21 12:23:15 -0700

    feat: add new round functionality
    Added in states for different elements for during different states in the game.
    
    Also added in scoring after a round is completed

--- game.js
+++ game.js
@@ -1,12 +1,89 @@
-let cardsContainer = document.querySelector("#js-cards-container");
-let maxCards       = 0;
-let cardNumber     = 0;
+const cardsContainer = document.querySelector("#js-cards-container");
+const scoreRoundBtn  = document.querySelector("#js-score-round");
+const nextRoundBtn   = document.querySelector("#js-next-round");
+const newGameBtn     = document.querySelector("#js-new-game");
+const scoreContainer = document.querySelector("#js-score");
 
-document.addEventListener("DOMContentLoaded", function(event) {
+let maxCards   = 0;
+let cardNumber = 0;
+let score      = 0;
+let letter     = 'e';
+
+/**
+ * ----------------------------------------------
+ * Add Event Listeners
+ * ----------------------------------------------
+ */
+
+// Start a new game
+document.addEventListener("DOMContentLoaded", startNewGame);
+newGameBtn.addEventListener("click", startNewGame);
+
+// Score a single round
+scoreRoundBtn.addEventListener("click", beginScoringRound);
+
+// Move on to the next round
+nextRoundBtn.addEventListener("click", finishScoringRound);
+
+/**
+ * ----------------------------------------------
+ * Event Callbacks
+ * ----------------------------------------------
+ */
+
+// Starts a new game
+function startNewGame() {
+  score = 0;
+
+  startNewRound();
+}
+
+function startNewRound() {
+  removePreviousCard();
+
   maxCards   = data.length;
   cardNumber = getNewCard();
   loadCard();
-});
+
+  // Reset interactable elements
+  enableAllInClass('.card__answer');
+  disableAllInClass('.card__was-taken');
+  disableAllInClass('label');
+
+  scoreRoundBtn.style.display = 'inline';
+  nextRoundBtn.style.display = 'none';
+}
+
+// Disable changing answers, and enable elements for scoring rounds
+function beginScoringRound(event) {
+  disableAllInClass('.card__answer');
+  enableAllInClass('.card__was-taken');
+  enableAllInClass('label');
+
+  // Hide the score round button and display the next round button
+  scoreRoundBtn.style.display = 'none';
+  nextRoundBtn.style.display = 'inline';
+}
+
+function finishScoringRound(event) {
+  var answers = document.querySelectorAll('.card__answer');
+  answers.forEach((answer, index) => {
+    const answerParts = answer.value.split(' ');
+    answerParts.forEach((answerPart, innerIndex) => {
+      if (answerPart.startsWith(letter)) {
+        score++;
+      }
+    });
+  });
+  scoreContainer.innerText = score;
+  startNewRound();
+}
+
+/**
+ * ----------------------------------------------
+ * Helper Methods
+ * ----------------------------------------------
+ */
 
 // Get a new card to display
 function getNewCard() {
@@ -15,18 +92,36 @@ function getNewCard() {
 
 // load card into the game "board"
 function loadCard() {
-  data[cardNumber].forEach((row) => {
+  data[cardNumber].forEach((row, index) => {
     cardsContainer.innerHTML += `
       <div class="card">
         <div class="card__title">${row}</div>
         <input class="card__answer" />
-        <input class="card__was-taken" type="checkbox" />
+        <div class="card__taken">
+          <label for="card-taken-${index}">Yes</label>
+          <input id="card-taken-${index}" class="card__was-taken" type="checkbox" />
+        </div>
       </div>
     `;
   });
 }
 
-// Load a single row of a card
-function loadCardRow() {
+// Disable all elements with a given selector
+function disableAllInClass(selector) {
+  var elements = document.querySelectorAll(selector);
+  elements.forEach((element, index) => {
+    element.setAttribute('disabled', 'disabled');
+  });
+}
 
+// Enable all the elements with a given selector
+function enableAllInClass(selector) {
+  var elements = document.querySelectorAll(selector);
+  elements.forEach((element, index) => {
+    element.removeAttribute('disabled');
+  });
+}
+
+function removePreviousCard() {
+  cardsContainer.innerHTML = ""; 
 }
